91b98a3f423c8e712ccdc6a723f25d841cb7ee50
Krati Mittal <krati@nickelfox.co>
2024-01-11 16:09:21 +0530

    feat(home): add reset progress button

--- ToolIdentification/Sources/ToolIdentification/ToolIdentification.swift
+++ ToolIdentification/Sources/ToolIdentification/ToolIdentification.swift
@@ -11,64 +11,65 @@ struct ScreenSize {
     static let height = UIScreen.main.bounds.size.height
 }
 
-    public struct HomeView: View {
-        
-        let primaryGradientColor = Color(red: 0.137,
-                                         green: 0.121,
-                                         blue: 0.125)
-        public init() {}
-        
-        public var body: some View {
-            ZStack {
-                /// Background Image view
-                Image("HomeIcon") // Fixme: Required to be configured
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                
-                LinearGradient(colors: [primaryGradientColor.opacity(0.82),
-                                        primaryGradientColor.opacity(0.74),
-                                        primaryGradientColor.opacity(0.68),
-                                        primaryGradientColor.opacity(0.48),
-                                        primaryGradientColor.opacity(0.41),
-                                        primaryGradientColor.opacity(0)],
-                               startPoint: .top,
-                               endPoint: .bottom)
-                
-                VStack {
-                    VStack(spacing: 65) {
-                        /// App specific logo icon with edition details
-                        Image("IFSTAIcon") // Fixme: Required to be configured
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 0.5 * ScreenSize.width)
+public struct HomeView: View {
+    
+    let primaryGradientColor = Color(red: 0.137,
+                                     green: 0.121,
+                                     blue: 0.125)
+    public init() {}
+    
+    public var body: some View {
+        ZStack {
+            /// Background Image view
+            Image("HomeIcon") // Fixme: Required to be configured
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+            
+            LinearGradient(colors: [primaryGradientColor.opacity(0.82),
+                                    primaryGradientColor.opacity(0.74),
+                                    primaryGradientColor.opacity(0.68),
+                                    primaryGradientColor.opacity(0.48),
+                                    primaryGradientColor.opacity(0.41),
+                                    primaryGradientColor.opacity(0)],
+                           startPoint: .top,
+                           endPoint: .bottom)
+            
+            VStack {
+                VStack(spacing: 65) {
+                    /// App specific logo icon with edition details
+                    Image("IFSTAIcon") // Fixme: Required to be configured
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 0.5 * ScreenSize.width)
+                    
+                    VStack(spacing: 20) {
+                        Text("Identify")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundStyle(.white)
+                            .frame(height: 50)
                         
-                        VStack(spacing: 20) {
-                            Text("Identify")
-                                .font(.largeTitle)
-                                .fontWeight(.bold)
-                                .foregroundStyle(.white)
-                                .frame(height: 50)
-                            
-                            Capsule()
-                                .frame(width: 55, height: 7)
-                                .foregroundStyle(.white)
-                        }
+                        Capsule()
+                            .frame(width: 55, height: 7)
+                            .foregroundStyle(.white)
+                    }
+                }
+                
+                Spacer()
+                
+                VStack(spacing: 65) {
+                    VStack(spacing: 28) {
+                        Text("Container Identification")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundStyle(.white)
+                        
+                        Text("Remaining Questions :  157/ 157")
+                            .fontWeight(.regular)
+                            .foregroundStyle(.white)
                     }
                     
-                    Spacer()
-                    
-                    VStack(spacing: 65) {
-                        VStack(spacing: 28) {
-                            Text("Container Identification")
-                                .font(.largeTitle)
-                                .fontWeight(.bold)
-                                .foregroundStyle(.white)
-                            
-                            Text("Remaining Questions :  157/ 157")
-                                .fontWeight(.regular)
-                                .foregroundStyle(.white)
-                        }
-                        
+                    VStack(spacing: 30) {
                         Button(action: {
                             
                         }, label: {
@@ -79,12 +80,25 @@ struct ScreenSize {
                                 .background(.yellow)
                                 .cornerRadius(10)
                         })
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            Text("Reset Progress Reset Progress")
+                                .fontWeight(.bold)
+                                .foregroundStyle(.black)
+                                .frame(height: 0.06 * ScreenSize.height)
+                                .padding(.horizontal, 20)
+                                .background(.yellow)
+                                .cornerRadius(10)
+                        })
                     }
-                    
-                    Spacer()
                 }
-                .padding(.top, 0.20 * ScreenSize.width)
+                
+                Spacer()
             }
-            .ignoresSafeArea()
+            .padding(.top, 0.20 * ScreenSize.width)
         }
+        .ignoresSafeArea()
     }
+}
