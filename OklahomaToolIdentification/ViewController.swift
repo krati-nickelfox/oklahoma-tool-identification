55262eb0cdf1528a09f267895bae87db819ec76a
chakshu-nickelfox <chakshu@nickelfox.com>
2024-02-22 21:47:55 +0530

    feat(OKLHM-3342): create static reports listing ui

--- OklahomaToolIdentification/ViewController.swift
+++ OklahomaToolIdentification/ViewController.swift
@@ -27,13 +27,7 @@ class ViewController: UIViewController {
         ToolIdentification.quizManager?.initializeQuiz()
         
         // Add home view from the package
-        let swiftUIView = HomeView(
-            backgroundImageName: "identify-home",
-            appLogoName: "logo",
-            practiceExamIcon: "practice-exam",
-            studyDeckIcon: "reports",
-            reportsIcon: "study-deck"
-        )
+        let swiftUIView = ReportsView()
         let vc = UIHostingController(rootView: swiftUIView)
         vc.view.frame = self.view.bounds
         vc.view.backgroundColor = UIColor.clear
---
+++ ToolIdentification/Sources/ToolIdentification/Views/Reports/ReportsView.swift
@@ -0,0 +1,62 @@
+//
+//  ReportsView.swift
+//
+//
+//  Created by Chakshu Dawara on 22/02/24.
+//
+
+import SwiftUI
+
+public struct ReportsView: View {
+    
+    /// Environment Variable
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    public init() { }
+    
+    // MARK: Body
+    public var body: some View {
+        ZStack {
+            VStack {
+                HeaderView(title: "Placards Reports", leftButtonAction: {
+                    presentationMode.wrappedValue.dismiss()
+                }, rightButtonAction: {}, leftIconName: "back-icon", rightIconName: "ExitIcon")
+                subcategoriesProgressView
+            }
+        }
+        .background(Color(red: 35/255, green: 31/255, blue: 32/255))
+    }
+    
+    var subcategoriesProgressView: some View {
+        ScrollView(showsIndicators: false, content: {
+            VStack(spacing: 16, content: {
+                ForEach(0..<10) { num in
+                    ZStack(alignment: .center) {
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(Color.black, lineWidth: 1)
+                        
+                        HStack(spacing: 16) {
+                            Text("Subcategory\(num)")
+                                .font(.caption)
+                                .foregroundColor(.white)
+                            
+                            Text("80.0%")
+                                .font(.caption)
+                                .foregroundColor(.white)
+                            
+                            ProgressView(value: 0.5)
+                                .progressViewStyle(LinearProgressViewStyle())
+                                .frame(height: 10)
+                                .scaleEffect(x: 1, y: 2.5, anchor: .center)
+                                .tint(.red)
+                        }
+                        .padding(.horizontal, 16)
+                    }
+                    .background(LinearGradient(colors: [Color(red: 27/255, green: 24/255, blue: 25/255), Color(red: 35/255, green: 31/255, blue: 32/255)], startPoint: .leading, endPoint: .trailing))
+                    .frame(height: 31)
+                }
+            })
+            .padding(.horizontal, 12)
+        })
+    }
+}
